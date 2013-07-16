b4e8795c38437c6d4e736409633400eab5b7c4f5
kthsdwwl <kthsdwwl@gmil.com>
2013-07-16 16:52:53 +0800

    fix

--- test/integration/user_stories_test.rb
+++ test/integration/user_stories_test.rb
@@ -2,36 +2,39 @@ require 'test_helper'
 
 class UserStoriesTest < ActionDispatch::IntegrationTest
   fixtures :products
-  
+  # test "the truth" do
+  #   assert true
+  # end
+
   LineItem.delete_all
   Order.delete_all
   ruby_book = products(:ruby)
-  
+
   get "/"
   assert_response :success
   assert_template "index"
-  
+
   xml_http_request :post, '/line_items', product_id: ruby_book.id
   assert_response :success
-  
+
   cart = Cart.find(session[:cart_id])
   assert_equal 1, cart.line_items.size
   assert_equal ruby_book, cart.line_items[0].product
-  
+
   get "orders/new"
   assert_response :success
   assert_template "new"
-  
+
   post_via_redirect "/orders",
-                    order: { name:    "Dave Thomas",
-                             address: "123 The Street",
-                             email:   "dava@example.com",
-                             pay_type:"Check" }
+                  order: {  name:     "Dave Thomas",
+                            address:  "123 The Street",
+                            email:    "dave@example.com",
+                            pay_type: "Check" }
   assert_response :success
   assert_template "index"
   cart = Cart.find(session[:cart_id])
   assert_equal 0, cart.line_items.size
-  
+
   orders = Order.all
   assert_equal 1, orders.size
   order = orders[0]
@@ -50,3 +53,4 @@ class UserStoriesTest < ActionDispatch::IntegrationTest
   assert_equal 'Sam Ruby <depot@example.com>', mail[:from].value
   assert_equal "Pragmatic Store Order Confirmation", mail.subject
 end
+
