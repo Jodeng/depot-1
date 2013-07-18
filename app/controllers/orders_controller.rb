73c3ea4775b07fd7ea251a27a126791e09a59f96
郑培基 <hornfezonter@foxmail.com>
2013-07-18 15:01:53 +0800

    mailer

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -43,6 +43,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id]);
         session[:cart_id] = nil
+        OrderNotifier.received(@order).deliver
 
         format.html { redirect_to store_path, notice: 'Thank you for your order' }
         format.json { render action: 'show', status: :created, location: @order }
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Depot::Application.initialize!
+
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    address: "smtp.qq.com",
+    port: 25,
+    #domain: "domain.of.sender.net",
+    authentication: "plain",
+    user_name: "",
+    password: "",
+    enable_starttls_auto: true
+  }
+end
